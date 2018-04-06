8b8d549069780006fbf7066fc99803fb9612cb74
mike connor <mike@rocketnumber9.org>
2018-04-06 14:08:51 -0500

    quit if no id found

--- inject-durly-parent.js
+++ inject-durly-parent.js
@@ -1,25 +1,29 @@
 (function() {
+  if (document.getElementById("evidon-vpaid")) {
     var durlyParmaString = document
-        .getElementById("evidon-vpaid")
-        .src.split("?")[1];
+      .getElementById("evidon-vpaid")
+      .src.split("?")[1];
     var adIFrame = window.frameElement;
     var durlyScript = document.createElement("SCRIPT");
     durlyScript.setAttribute("type", "text/javascript");
     durlyScript.setAttribute("data-name", "durly");
     if (adIFrame.clientWidth) {
-        durlyParmaString = durlyParmaString.concat(
-            ";ad_w=" + adIFrame.clientWidth
-        );
+      durlyParmaString = durlyParmaString.concat(
+        ";ad_w=" + adIFrame.clientWidth
+      );
     }
     if (adIFrame.clientHeight) {
-        durlyParmaString = durlyParmaString.concat(
-            ";ad_h=" + adIFrame.clientHeight
-        );
+      durlyParmaString = durlyParmaString.concat(
+        ";ad_h=" + adIFrame.clientHeight
+      );
     }
     durlyParmaString = durlyParmaString.concat(";vpaid=true");
     durlyScript.setAttribute(
-        "src",
-        "https://dev.betrad.com/durly.js?" + durlyParmaString
+      "src",
+      "https://dev.betrad.com/durly.js?" + durlyParmaString
     );
     adIFrame.contentWindow.document.body.appendChild(durlyScript);
+  } else {
+    console.warn("No script element found with ‘evidon-vpaid’ identifier");
+  }
 })();
